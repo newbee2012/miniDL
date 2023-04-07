73af3e31cdcdb1ff40aaefa1a1986a25ab972f33
陈德嘉 <chendejia@xiaomi.com>
2023-04-07 20:39:26 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -13,4 +13,5 @@ g++ -Winline -Wfatal-errors -fexceptions -O3 -Iinclude -Iextern/include -c /home
 g++ -Winline -Wfatal-errors -fexceptions -O3 -Iinclude -Iextern/include -c /home/chendejia/workspace/github/miniDL/src/pool_layer.cpp -o obj/Release/src/pool_layer.o
 g++ -Winline -Wfatal-errors -fexceptions -O3 -Iinclude -Iextern/include -c /home/chendejia/workspace/github/miniDL/src/relu_layer.cpp -o obj/Release/src/relu_layer.o
 g++ -Winline -Wfatal-errors -fexceptions -O3 -Iinclude -Iextern/include -c /home/chendejia/workspace/github/miniDL/src/softmax_layer.cpp -o obj/Release/src/softmax_layer.o
+mkdir -p bin/Release
 g++ -Lextern/lib -o bin/Release/miniDL obj/Release/src/conv_layer.o obj/Release/src/data.o obj/Release/src/full_connect_layer.o obj/Release/src/input_layer.o obj/Release/src/layer.o obj/Release/src/main.o obj/Release/src/net_model.o obj/Release/src/net_model_binary.o obj/Release/src/neuron.o obj/Release/src/pool_layer.o obj/Release/src/relu_layer.o obj/Release/src/softmax_layer.o  -O3 -static-libstdc++ -static -s  -lboost_system -lpthread -ljsoncpp
